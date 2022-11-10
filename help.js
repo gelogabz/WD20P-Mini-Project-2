0bfdcbeb27098eaae8d9ab835f377809706dc071
Vien08 <113112152+Vien08@users.noreply.github.com>
2022-11-11 02:08:21 +0800

    Help Page
    Updated Suggestion form but not necessarily done

--- help.js
+++ help.js
@@ -10,73 +10,193 @@
 // });
 
 
-// const form = document.getElementById('form');
-// const firstname = document.getElementById('fname');
-// const lastname = document.getElementById('lname');
-// const email = document.getElementById('email');
-// const contact = document.getElementById('contact');
-// const message = document.getElementById('mess');
+const closebtn = function (){
+  let x = document.getElementById('close')
 
-//   form.addEventListener('submit', (e) => {
-//     e.preventDefault();
-//       checkInputs();
-      
-//   })
-
-//     function checkInputs() {
-//       const fvalue = firstname.value.trim();
-//       const lvalue =lastname.value.trim();
-//       const evalue =email.value.trim();
-//       const cvalue =contact.value.trim();
-//       const mvalue =message.value.trim();
-
-//       if (fvalue === ' '){
-//         setErrorFor(firstname, '')
-//       } else{
-//         setSuccessFor(username);
-//       } 
-//     }
+   location.reload();
+}
 
 
-const btn = function(){
-    let firstname = document.getElementById('fname').value
-    let lastname = document.getElementById('lname').value
-    let email = document.getElementById('email').value
-    let contact = document.getElementById('contact').value
-    let message = document.getElementById('mess').value
 
 
-      // if (firstname ===""){
-      //   alert('Please input First name')
-      // }
-      // else if (lastname ===""){
-      //   alert('Please input Last name')
-      // }
-      // else if (email ===""){
-      //   alert('Please input Email')
-      // }
-      // else if (contact ===""){
-      //   alert('Please input Contact No:')
-      // }
-      // else if (message ===""){
-      //   alert('Please input Message')
-      // }
-      // else{
-      //   alert('The form is Submitted')
-      // }
-
+const yourname = function(){
+  let firstname = document.getElementById('fname').value.trim();
 
       if (firstname ===""){
         fname.style.borderColor = "red";
-        alert('Please input Your Name')
-       
+        fname.style.borderWidth = "2px";
+        document.getElementById('error1').innerHTML = "Input Required !"
       }
       else{
         fname.style.borderColor = "green";
+        fname.style.borderWidth = "2px";
+        document.getElementById('error1').innerHTML = " "
       }
-
-  // alert('The form is Submitted')
 }
+
+const Lastname = function(){
+  let lastname = document.getElementById('lname').value.trim();
+
+  if (lastname ===""){
+    lname.style.borderColor = "red";
+    lname.style.borderWidth = "2px";
+    document.getElementById('error2').innerHTML = "Input Required !"
+  }
+  else{
+    lname.style.borderColor = "green";
+    lname.style.borderWidth = "2px";
+    document.getElementById('error2').innerHTML = " "
+  }
+}
+
+const emailko = function(){
+  let email = document.getElementById('inputEmail').value.trim();
+
+  if (email ===""){
+    inputEmail.style.borderColor = "red";
+    inputEmail.style.borderWidth = "2px";
+    document.getElementById('error3').innerHTML = "Input Required !"
+  }
+  else{
+    inputEmail.style.borderColor = "green";
+    inputEmail.style.borderWidth = "2px";
+    document.getElementById('error3').innerHTML = " "
+  }
+}
+
+const contactNo = function(){
+  let contact = document.getElementById('contacts').value.trim();
+
+      if (contact ===""){
+        contacts.style.borderColor = "red";
+        contacts.style.borderWidth = "2px";
+        document.getElementById('error4').innerHTML = "Input Required !"
+      }
+      else{
+        contacts.style.borderColor = "green";
+        contacts.style.borderWidth = "2px";
+        document.getElementById('error4').innerHTML = " "
+      }
+}
+
+const message = function(){
+  let messCom = document.getElementById('mess').value.trim();
+
+      if (messCom ===""){
+        mess.style.borderColor = "red";
+        mess.style.borderWidth = "2px";
+        document.getElementById('error5').innerHTML = "Input Required !"
+      }
+      else{
+        mess.style.borderColor = "green";
+        mess.style.borderWidth = "2px";
+        document.getElementById('error5').innerHTML = " "
+      }
+}
+
+
+
+
+const btn = function(){
+  let firstname = document.getElementById('fname').value.trim();
+  let lastname = document.getElementById('lname').value.trim();
+  let email = document.getElementById('inputEmail').value.trim();
+  let contact = document.getElementById('contacts').value.trim();
+  let messCom = document.getElementById('mess').value.trim();
+  // let all = firstname + lastname + email + contact;
+  // let error = "Input Required !"
+    
+    if (firstname === ""  &&  lastname === ""){
+      fname.style.borderColor = "red";
+      fname.style.borderWidth = "2px";
+      document.getElementById('error1').innerHTML = "Input Required !"
+    }
+
+    else if (lastname === ""){
+      lname.style.borderColor = "red";
+      lname.style.borderWidth = "2px";
+      document.getElementById('error2').innerHTML = "Input Required !"
+    }
+
+    else if (email === ""){
+      inputEmail.style.borderColor = "red";
+      inputEmail.style.borderWidth = "2px";
+      document.getElementById('error3').innerHTML = "Input Required !"
+    }
+
+    else if (contact === ""){
+      contacts.style.borderColor = "red";
+      contacts.style.borderWidth = "2px";
+      document.getElementById('error4').innerHTML = "Input Required !"
+    }
+
+    else if (messCom === ""){
+      mess.style.borderColor = "red";
+      mess.style.borderWidth = "2px";
+      document.getElementById('error5').innerHTML = "Input Required !"
+    }
+    else{
+      alert('success');
+      location.reload();
+    }
+}
+
+
+
+
+
+// let firstname = document.getElementById('fname').value.trim();
+// let lastname = document.getElementById('lname').value.trim();
+// let email = document.getElementById('inputEmail').value.trim();
+// let contact = document.getElementById('contacts').value.trim();
+// let messCom = document.getElementById('mess').value.trim();
+
+//   if (firstname !== ""){
+//     fname.style.borderColor = "green";
+//     fname.style.borderWidth = "2px";
+//   }
+
+// const btn = function(){
+
+
+//   if (firstname !== ""){
+//     fname.style.borderColor = "green";
+//     fname.style.borderWidth = "2px";
+//   }
+//   else if (lastname !== ""){
+//     lname.style.borderColor = "green";
+//     lname.style.borderWidth = "2px";
+//   }
+//   else if (email !== ""){
+//     inputEmail.style.borderColor = "green";
+//     inputEmail.style.borderWidth = "2px";
+//   }
+//   else if (contact !== ""){
+//     contacts.style.borderColor = "green";
+//     contacts.style.borderWidth = "2px";
+//   }
+//   else if (messCom !== ""){
+//     mess.style.borderColor = "green";
+//     mess.style.borderWidth = "2px";
+//   }
+//   else{
+//     fname.style.borderColor = "red";
+//     fname.style.borderWidth = "2px";
+//     document.getElementById('error1').innerHTML = "Input Required !"
+//     lname.style.borderColor = "red";
+//     lname.style.borderWidth = "2px";
+//     document.getElementById('error2').innerHTML = "Input Required !"
+//     inputEmail.style.borderColor = "red";
+//     inputEmail.style.borderWidth = "2px";
+//     document.getElementById('error3').innerHTML = "Input Required !"
+//     contacts.style.borderColor = "red";
+//     contacts.style.borderWidth = "2px";
+//     document.getElementById('error4').innerHTML = "Input Required !"
+//     mess.style.borderColor = "red";
+//     mess.style.borderWidth = "2px";
+//     document.getElementById('error5').innerHTML = "Input Required !"
+//   }
+// }
 
 // let count = 0;
 
@@ -85,6 +205,14 @@ const btn = function(){
 //   console.log(count);
 // }
 
+// const submit(){
+// 	let x = document.getElementbyId('firstname').value.trim();
+// let y = document.getElementbyId('variable')
+	
+// 	if ( x !== "")
+// 	alert('Success')
+
+// }
 
 
 
